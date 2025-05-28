2c0ba00ec0b7e8fbd6cbc7d050858aecb606bf16
Ahmed Fares <ahmedfares659@gmail.com>
2025-05-29 02:28:17 +0300

    add get route

--- routes/paymentRoutes.js
+++ routes/paymentRoutes.js
@@ -6,6 +6,7 @@ import {
 
 const router = express.Router();
 
+router.get('/paymob/callback', handlePaymobCallback);
 router.post('/paymob/callback', handlePaymobCallback);
 
 export default router; 
